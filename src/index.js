a549c6e9afe8bc55651521deefdc2c0c128ccfb3
katerynazhvanko <kateryna.tertychna@gmail.com>
2023-04-30 12:32:56 +0300

    task-2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles/normalize.css";
 import "./styles/index.css";
 import { fetchAllUsers } from "./requests/users";
-import { fetchAllProducts } from "./requests/products";
+import { fetchAllProducts, fetchProductByID } from "./requests/products";
 import { fetchAllPosts } from "./requests/posts";
-import { createMarkupAllProducts } from "./services/markupService";
+import {
+  createMarkupAllProducts,
+  createMarkupProduct,
+} from "./services/markupService";
 
 const allProductsRef = document.querySelector("#allProducts");
 
@@ -15,4 +18,22 @@ async function getAllProducts() {
   allProductsRef.innerHTML = createMarkupAllProducts(products);
 }
 
-getAllProducts();
+// getAllProducts();
+
+const form = document.querySelector("#singleProductForm");
+const div = document.querySelector("#singleProduct");
+form.addEventListener("submit", onGetProductByID);
+
+async function onGetProductByID(event) {
+  event.preventDefault();
+  const id = event.target.elements.id.value.trim();
+  if (!id) {
+    return;
+  }
+  try {
+    const { data } = await fetchProductByID(id);
+    div.innerHTML = createMarkupProduct(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
